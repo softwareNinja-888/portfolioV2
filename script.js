791a9b3e2e998e483e00230fec79e9b6c6b1e767
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-08-16 22:53:27 +0200

    Add responsive header

---
+++ script.js
@@ -0,0 +1,8 @@
+// (function Start(){
+//     const menu = document.querySelector(".menuCon")
+//     const nav = document.querySelector(".linksCon")
+
+//     console.log(menu);
+//     menu.addEventListener("click",()=>{
+//     })
+// })()
